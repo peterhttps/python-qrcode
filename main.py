d224f8ca914f40d5a8c00e32e62c4da3f7865b22
Pedro Lucas <plrf100@gmail.com>
2021-09-15 23:28:35 -0300

    feature(show): adding image show

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import qrcode
+from PIL import Image
 
 inputText = input("Tell a qrcode information:\n> ")
 
 img = qrcode.make(inputText)
 type(img)  
 img.save("some_file.png")
+im = Image.open("some_file.png")
+im.show()
